c4d101f5f8e65f47a3ce3e93a92c7194af649368
Kyome22 <kyomesuke@gmail.com>
2021-01-21 15:21:24 +0900

    xcuserstateを除外

--- Pods/Target Support Files/Pods-Desktop Cleaner/Pods-Desktop Cleaner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Desktop_Cleaner : NSObject
-@end
-@implementation PodsDummy_Pods_Desktop_Cleaner
-@end
--- Pods/Target Support Files/Pods-Desktop CleanerUITests/Pods-Desktop CleanerUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Desktop_CleanerUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Desktop_CleanerUITests
-@end
